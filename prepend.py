73c5d0534ccb878d5623320412c95eabf58cf408
Devansh Khare <devanshkhare0@gmail.com>
2025-06-25 22:54:26 +0530

    linked list append and prepend

---
+++ prepend.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __str__(self):
+        result = "Linked List: "
+        current = self.head
+        while current is not None:
+            result += str(current.value)
+            if current.next is not None:
+                result += " -> "
+            current = current.next
+        return result
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+ll = LinkedList()
+ll.append(10)
+ll.append(20)
+ll.append(30)
+
+print(ll)
+ll.prepend(40)
+print(ll)    
