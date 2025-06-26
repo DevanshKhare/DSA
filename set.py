8377c5255d1c5485c8b12f7730b0922974f89a85
Devansh Khare <devanshkhare0@gmail.com>
2025-06-26 23:04:18 +0530

    linked list getter and setter

---
+++ set.py
@@ -0,0 +1,93 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __str__(self):
+        result = "Linked List: "
+        current= self.head
+        while current is not None:
+            result += str(current.value)
+            if current.next is not None:
+                result += " -> "
+            current = current.next
+        return result
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+    
+    def insert(self, index, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            new_node.next = temp.next
+            temp.next = new_node
+        self.length += 1
+
+    def search(self, value):
+        current = self.head
+        index = 0
+        while current is not None:
+            if current.value == value:
+                return index
+            current = current.next
+            index+=1
+        return -1
+    
+    def get(self, index):
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current.value
+    
+    def set(self, index, value):
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        current.value = value
+    
+ll = LinkedList()
+ll.append(10)
+ll.append(20)
+ll.append(30)
+ll.append(40)
+ll.append(50)
+ll.prepend(5)
+print(ll)
+ll.insert(5,45)
+print(ll)
+print(ll.search(50))
+print(ll.get(5))
+ll.set(3, 200)
+print(ll)
