9c78db7bd5c9fb03706123acaf93aa173e2d7a15
Devansh Khare <devanshkhare0@gmail.com>
2025-07-05 10:56:02 +0530

    linked list remove duplicate

---
+++ removeDuplicate.py
@@ -0,0 +1,161 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __str__(self):
+        result = "Linked List: "
+        current= self.head
+        while current is not None:
+            result += str(current.value)
+            if current.next is not None:
+                result += " -> "
+            current = current.next
+        return result
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+    
+    def insert(self, index, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            new_node.next = temp.next
+            temp.next = new_node
+        self.length += 1
+
+    def search(self, value):
+        current = self.head
+        index = 0
+        while current is not None:
+            if current.value == value:
+                return index
+            current = current.next
+            index+=1
+        return -1
+    
+    def get(self, index):
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current
+
+    def set(self, index, value):
+        temp =  self.get(index)
+        if temp.value is not None:
+            temp.value = value
+    
+    def pop(self):
+        popped_node = self.head
+        self.head = popped_node.next
+        self.length -= 1
+
+    def pop_last(self):
+        popped_node = self.tail
+        temp = self.head
+        while temp.next is not popped_node:
+            temp = temp.next
+        temp.next = None
+        self.tail = temp
+        self.length -= 1
+
+    def remove(self, index):
+        prev = self.get(index-1)
+        popped_node = prev.next
+        prev.next = popped_node.next
+        popped_node.next = None
+        self.length -= 1
+
+    def reverse(self):
+        prev_node = None
+        current_node = self.head
+        while current_node is not None:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        self.head, self.tail = self.tail, self.head
+
+    def middle(self):
+        current = self.head
+        for _ in range(self.length//2):
+            current = current.next
+        return current.value
+
+    def middle2(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow.value
+
+    def remove_duplicate(self):
+        unique = set()
+        current = self.head
+        unique.add(current.value)
+        while current.next is not None:
+            if current.next.value in unique:
+                current.next = current.next.next
+                self.length -= 1
+            else:
+                unique.add(current.next.value)
+                current = current.next
+
+ll = LinkedList()
+ll.append(10)
+ll.append(20)
+ll.append(30)
+ll.append(40)
+ll.append(30)
+ll.append(40)
+ll.append(50)
+ll.prepend(5)
+print(ll)
+ll.insert(5,45)
+print(ll)
+print(ll.search(50))
+print(ll.get(5))
+ll.pop()
+print(ll)
+ll.pop_last()
+print(ll)
+# ll.remove(3)
+# ll.remove(1)
+print(ll)
+ll.reverse()
+print(ll)
+print(ll.middle2())
+ll.remove_duplicate()
+print(ll)
