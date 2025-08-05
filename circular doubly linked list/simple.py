dc71c753c6cf84c87275e18afbebb75c4936243f
Devansh Khare <devanshkhare0@gmail.com>
2025-08-05 09:06:26 +0530

    simpled dc linked list

---
+++ circular doubly linked list/simple.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class CirularDoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        new_node.next = new_node
+        new_node.prev = new_node
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+ll = CirularDoublyLinkedList(10)
+print(ll.length)
