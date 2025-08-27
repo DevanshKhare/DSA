b4a0fc19b6cd21205c6bc958ae1ec096a98468d6
Devansh Khare <devanshkhare0@gmail.com>
2025-08-27 14:45:07 +0530

    simple queue

---
+++ queue/simple.py
@@ -0,0 +1,10 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def __str__(str):
+        values = [str(x) for x in self.items]
+        return " ".join(values)
+
+    def is_empty(self):
+        return len(self.items) == 0
