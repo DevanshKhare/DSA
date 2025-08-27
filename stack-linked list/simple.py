e92ab4ee72c147249f968d7df19a71d2f76e546a
Devansh Khare <devanshkhare0@gmail.com>
2025-08-27 10:36:34 +0530

    linked list impolementation of stack push method

---
+++ stack-linked list/simple.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, values):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.length = 0
+
+new_stack = Stack()
