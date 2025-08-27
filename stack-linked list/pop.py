63718df76490dc734fb07d7497b2a38372ad9907
Devansh Khare <devanshkhare0@gmail.com>
2025-08-27 10:41:36 +0530

    linked list impolementation of stack pop method

---
+++ stack-linked list/pop.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.length = 0
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.top is None:
+            return None
+        popped_node = self.top
+        self.top = popped_node.next
+        popped_node.next = None
+        self.length -= 1
+        return popped_node
+
+new_stack = Stack()
+new_stack.push(10)
+new_stack.push(20)
+new_stack.push(30)
+new_stack.push(40)
+new_stack.push(50)
+print(new_stack.top.value)
+print(new_stack.pop().value)
