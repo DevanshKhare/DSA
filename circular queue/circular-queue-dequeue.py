71371854c8a3b07ad0260a8598ccd154dde7119c
Devansh Khare <devanshkhare0@gmail.com>
2025-08-27 16:23:33 +0530

    printing circular queue

--- circular queue/circular-queue-dequeue.py
+++ circular queue/circular-queue-dequeue.py
@@ -5,6 +5,19 @@ class CircularQueue:
         self.front = -1
         self.rear = -1
     
+    def __str__(self):
+        if self.is_empty():
+            return "Queue is empty"
+        
+        result = "Queue Elements: "
+        i = self.front
+        while True:
+            result += str(self.items[i]) + " "
+            if i == self.rear:
+                break
+            i = (i+1)%self.maxSize
+        return result
+
     def is_empty(self):
         return self.front == -1
     
@@ -36,14 +49,19 @@ class CircularQueue:
 cq = CircularQueue(5)
 print(cq.is_empty())
 print(cq.is_full())
+print(cq)
+
 print(cq.enqueue(10))
 print(cq.enqueue(20))
 print(cq.enqueue(30))
 print(cq.enqueue(40))
 print(cq.enqueue(50))
+print(cq)
+
 print(cq.is_empty())
 print(cq.is_full())
 print(cq.dequeue())
 print(cq.dequeue())
 print(cq.is_full())
 
+print(cq)
