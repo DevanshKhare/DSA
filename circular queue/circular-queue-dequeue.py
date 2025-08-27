0411c2e152285a3c6bddd2c0a1847fe098057558
Devansh Khare <devanshkhare0@gmail.com>
2025-08-27 16:15:33 +0530

    circular queue dequeue

---
+++ circular queue/circular-queue-dequeue.py
@@ -0,0 +1,49 @@
+class CircularQueue:
+    def __init__(self, maxSize):
+        self.items = maxSize * [None]
+        self.maxSize = maxSize
+        self.front = -1
+        self.rear = -1
+    
+    def is_empty(self):
+        return self.front == -1
+    
+    def is_full(self):
+        if (self.rear + 1) % self.maxSize == self.front:
+            return True
+        return False
+    
+    def enqueue(self, value):
+        if self.is_full():
+            return "Queue is full"
+        if self.is_empty():
+            self.front = 0
+        self.rear = (self.rear + 1) % self.maxSize
+        self.items[self.rear] = value
+        return f"Inserted {value}"
+
+    def dequeue(self):
+        if self.is_empty():
+            return "Queue is empty"
+        removed_element = self.items[self.front]
+        if self.front == self.rear:
+            self.front = -1
+            self.rear = -1
+        else:
+            self.front = (self.front + 1)%self.maxSize
+        return removed_element
+
+cq = CircularQueue(5)
+print(cq.is_empty())
+print(cq.is_full())
+print(cq.enqueue(10))
+print(cq.enqueue(20))
+print(cq.enqueue(30))
+print(cq.enqueue(40))
+print(cq.enqueue(50))
+print(cq.is_empty())
+print(cq.is_full())
+print(cq.dequeue())
+print(cq.dequeue())
+print(cq.is_full())
+
