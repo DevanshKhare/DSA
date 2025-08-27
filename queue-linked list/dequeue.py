0e0bcf1a2e831fbc0061ae6b11666ce672200ee5
Devansh Khare <devanshkhare0@gmail.com>
2025-08-27 20:35:57 +0530

    linked list implementation of queue dequeue

--- queue-linked list/dequeue.py
+++ queue-linked list/dequeue.py
@@ -9,6 +9,19 @@ class Queue:
         self.rear = None
         self.length = 0
 
+    def __str__(self):
+        result = "Queue: "
+        current = self.front
+        while current:
+            result += str(current.value)
+            if current.next:
+                result += " -> "
+            current = current.next
+        return result
+
+    def is_empty(self):
+        return self.front is None
+
     def enqueue(self, value):
         new_node = Node(value)
         if self.front is None:
@@ -19,8 +32,23 @@ class Queue:
             self.rear = new_node
         self.length += 1
 
-    def dequeue(self, value):
+    def dequeue(self):
         if self.front is None:
             return "Queue is empty"
         self.front = self.front.next
         self.length -= 1
+    
+    def peek(self):
+        return self.front
+
+lq = Queue()
+print(lq)
+lq.enqueue(10)
+lq.enqueue(20)
+lq.enqueue(30)
+lq.enqueue(40)
+lq.enqueue(50)
+print(lq)
+lq.dequeue()
+print(lq)
+print(lq.peek().value)
